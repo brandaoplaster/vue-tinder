b7a588857c82cbd479073e253fbe6127afd4590a
brandaoplaster <brandaoplaster@gmail.com>
2020-03-15 00:36:31 -0300

    Including imports of axios and Buefy in the main

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 Vue.config.productionTip = false
 
