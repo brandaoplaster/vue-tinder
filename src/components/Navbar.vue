6a0204b6dc7cf5fca8ff78aacab38f9b1451e40a
brandaoplaster <brandaoplaster@gmail.com>
2020-03-16 23:02:17 -0300

    Creating navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="#">
+				<h3 class="has-text-white">
+					Vue-Tinder
+				</h3>
+			</a>
+		</div>
+	</nav>
+</template>
+
+<style lang="scss" scoped>
+	nav {
+		background-color: #FF6666;
+	}
+</style>
