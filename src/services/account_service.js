2b53da68d31349036560222ee7e37ad807056303
brandaoplaster <brandaoplaster@gmail.com>
2020-03-19 23:08:08 -0300

    implementation of the signUp method in the service account to create a new account for the user

--- src/services/account_service.js
+++ src/services/account_service.js
@@ -4,5 +4,10 @@ export default {
   async login(email, password) {
     let response = await axios.post("users/sign_in", { user: { email: email, password: password } })
     return response.data;
+  },
+
+  async signUp(name, email, password, passwordConfirmation) {
+    let response = await axios.post("users", { name: name, email: email, password: password, passwordConfirmation: passwordConfirmation })
+    return response
   }
 }
