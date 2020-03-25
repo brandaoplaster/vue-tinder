8e50ce2ac05a25508e85868ce90a039396869776
brandaoplaster <brandaoplaster@gmail.com>
2020-03-24 23:54:40 -0300

    Updated Home component to call the UserMatch component and to manage the selection list and when to switch from one to the other

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="section">
+    <div class="container" v-if="currentSelection">
+      <user-match />
+    </div>
+    <div class="loading has-text-centered" v-else-if="loading">
+      <i class="fas fa-spinner fa-spin fa-4x"></i>
+      <h1 class="is-size-5">
+        Carregando lista de seleção
+      </h1>
+    </div>
+    <div class="empty-selection-list" v-else>
+      <h1 class="is-size-4 has-text-centered">
+        Não há mais usuários disponíveis para você no momento...
+      </h1>
+    </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+  section.section {
+    padding: 0.5em;
+
+    div.loading {
+      padding-top: 10em;
+
+      h1 {
+        margin-top: 1em;
+      }
+    }
+
+    div.empty-selection-list {
+      h1 {
+        margin-top: 7em;
+      }
+    }
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapState } from 'vuex';
 
 export default {
-  name: 'Home',
+
   components: {
-    HelloWorld
+    UserMatch: () => import('@/components/UserMatch'),
+  },
+
+  computed: {
+    ...mapState('Match', ['selectionList', 'loading', 'currentSelection'])
+  },
+
+  watch: {
+    loading() {
+      this.setCurrentSelection(0);
+    },
+    
+  },
+
+  methods: {
+    ...mapActions('Match', ['loadSelectionList', 'setCurrentSelection'])
+  },
+
+  mounted() {
+    this.loadSelectionList();
   }
+  
 }
 </script>
