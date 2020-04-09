2b8b48349b65b4116d9c60340fb213d7c47ace9d
brandaoplaster <brandaoplaster@gmail.com>
2020-04-08 22:15:13 -0300

    Implemented UserShow view and adding ..editorconfig

--- src/views/UserShow.vue
+++ src/views/UserShow.vue
@@ -1,9 +1,142 @@
 <template>
-    <div></div>
+  <div>
+    <div>
+      <swiper>
+        <swiper-slide v-for="photo in currentUser.photos" :key="photo.url">
+          <img :src="photo.url">
+        </swiper-slide>
+      </swiper>
+
+      <div v-if="isLoggedUser" class="columns is-mobile is-gapless is-centered action-buttons">
+        <div class="column is-12">
+          <router-link  to="/profile/edit" class="button is-pulled-left is-info">
+            <i class="fas fa-edit"></i>
+          </router-link>
+        </div>
+      </div>
+
+      <div v-else class="columns is-mobile is-gapless is-centered action-buttons">
+        <div class="column is-12">
+          <button class="button is-pulled-right" @click="backToPreviousPage()">
+            <i class="fas fa-reply"></i>
+          </button>
+        </div>
+      </div>
+
+      <div class="columns is-mobile is-gapless is-multiline user-info">
+        <div class="column is-8">
+          <h5 class="is-size-3">
+            <strong>
+              {{ currentUser.name }}
+            </strong>
+          </h5>
+          <h5 class="is-size-5 has-text-grey">
+            <strong>
+              <i class="fas fa-building"></i>
+            </strong>
+            {{ currentUser.company }}
+          </h5>
+          <h5 class="is-size-5 has-text-grey">
+            <strong>
+              <i class="fas fa-university"></i>
+            </strong>
+            {{ currentUser.college }}
+          </h5>
+        </div>
+
+        <div v-if="currentUser.distance" class="column is-4 distance">
+          <h4 class="has-right is-size-6">
+            <strong class="has-text-danger">
+              {{ currentUser.distance }} km
+              <i class="fas fa-map-market-alt has-text-grey"></i>
+            </strong>
+          </h4>
+        </div>
+
+        <div class="column is-12 has-text-centered">
+          <h5 v-if="currentUser.description" class="is-size-6 has-text-grey description">
+            {{ currentUser.description }}
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style lang="scss" scoped>
+  div.columns {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  div.action-buttons {
+    margin-top: -2rem;
+    position: absolute;
+    z-index: 9999;
+    width: 100%;
+
+    button, a {
+      border-radius: 9999px;
+      height: 3.5rem;
+      width: 3.5rem;
+    }
+  }
+
+  div.user-info {
+    margin-top: 2rem;
+  }
+
+  div.distance {
+    h4 {
+      margin-top: 0.5rem;
+    }
+  }
+
+  h5.description {
+    margin-top: 2em;
+  }
+</style>
+
 <script>
-export default {
-    
-}
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import { mapState } from 'vuex';
+import router from '../router';
+
+  export default {
+    comments: {
+      swiper,
+      swiperSlide
+    },
+
+    props: ['user'],
+
+    data: () => ({
+      currentUser: {},
+      isLoggedUser: false
+    }),
+
+    computed: {
+      ...mapState({
+        account: state => state.Account.account
+      })
+    },
+
+    watch: {
+      $router (to, from) {
+        if (!this.user) {
+          console.log();
+        } else {
+          this.currentUser = this.user;
+        }
+      }
+    },
+
+    methods: {
+      backToPreviousPage() {
+        router.go(-1);
+      }
+    },
+
+  }
 </script>
