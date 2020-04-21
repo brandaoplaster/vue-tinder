7d1527c2d448975f0db0dc1e0ec22a43fc3c3d96
brandaoplaster <brandaoplaster@gmail.com>
2020-04-20 22:29:13 -0300

    Adding geolocation component

---
+++ src/views/BlockGeolocation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="has-text-centered has-text-grey">
+    <b-icon pack="fas" icon="sad-cry" size="is-large"></b-icon>
+    <h1 class="subtitle has-text-grey">
+      Desculpe, mas não podemos continuar sem a sua localização habilitada
+    </h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    margin-top: 3rem;
+  }
+  h1 {
+    margin-top: 2rem;
+  }
+</style>
+
+<script>
+import store from '../stores';
+
+  export default {
+    mounted() {
+      store.dispatch('loadGeolocation');
+    }
+  }
+</script>
